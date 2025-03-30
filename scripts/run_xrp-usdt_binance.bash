bb54086d0f94e01710d653b098004a8430ea3538
Mohammad Salar Eini <136807177+bettercallsalar@users.noreply.github.com>
2025-03-30 16:36:59 +0200

    Add new scripts for BNB/USDT, BTC/USD, and XRP/USDT configurations and update ETH/USDT logging

--- scripts/run_xrp-usdt_binance.bash
+++ scripts/run_xrp-usdt_binance.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-# run_eth-usdt_binance.sh
+# run_xrp-usdt_binance.sh
 
 # Change directory to the project root where main.py is located.
 cd ..
 
 # Set environment variables for this specific configuration.
 export EXCHANGE="binance"
-export SYMBOL="ETH/USDT"
+export SYMBOL="XRP/USDT"
 export TIMEFRAMES="1w,3d,1d,12h,8h,6h,4h,2h,1h,30m,15m,5m,3m,1m"
 export MONGODB_URI="mongodb://admin:bgKdk,Qcj'Zp)~X6maDwfW@localhost:9631/"
 
@@ -14,4 +14,4 @@ export MONGODB_URI="mongodb://admin:bgKdk,Qcj'Zp)~X6maDwfW@localhost:9631/"
 #python3 main.py
 
 # Option 2: Run in the background and redirect output to a log file
-python3 main.py > eth_usdt_binance.log 2>&1 &
+python3 main.py > xrp_usdt_binance.log 2>&1 &
