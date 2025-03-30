149991f500e1c74ad921113765fccf69c46be88c
Mohammad Salar Eini <136807177+bettercallsalar@users.noreply.github.com>
2025-03-30 15:55:36 +0200

    Uncomment execution command in run_btc-usdt_binance.bash for background logging

--- scripts/run_btc-usdt_binance.bash
+++ scripts/run_btc-usdt_binance.bash
@@ -11,7 +11,7 @@ export TIMEFRAMES="1w,3d,1d,12h,8h,6h,4h,2h,1h,30m,15m,5m,3m,1m"
 export MONGODB_URI="mongodb://admin:bgKdk,Qcj'Zp)~X6maDwfW@localhost:9631/"
 
 # Option 1: Run interactively (logs appear in the terminal)
-python3 main.py
+#python3 main.py
 
 # Option 2: Run in the background and redirect output to a log file
-# python3 main.py > btc_usdt_binance.log 2>&1 &
+python3 main.py > btc_usdt_binance.log 2>&1 &
