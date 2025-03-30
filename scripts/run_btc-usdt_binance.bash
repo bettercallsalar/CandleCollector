64eea9cee44561d1930b05cb82d6e0470f9b2be8
Mohammad Salar Eini <136807177+bettercallsalar@users.noreply.github.com>
2025-03-30 15:46:43 +0200

    Update MongoDB URI in run_btc-usdt_binance.bash to use a different port

--- scripts/run_btc-usdt_binance.bash
+++ scripts/run_btc-usdt_binance.bash
@@ -4,11 +4,6 @@ cd ..
 export EXCHANGE="binance"
 export SYMBOL="BTC/USDT"
 export TIMEFRAMES="1w,3d,1d,12h,8h,6h,4h,2h,1h,30m,15m,5m,3m,1m"
-export MONGODB_URI="mongodb://admin:bgKdk,Qcj'Zp)~X6maDwfW@localhost:27017/"
-python3 main.py &
-export EXCHANGE="binance"
-export SYMBOL="ETH/USDT"
-export TIMEFRAMES="1w,3d,1d,12h,8h,6h,4h,2h,1h,30m,15m,5m,3m,1m"
-export MONGODB_URI="mongodb://admin:bgKdk,Qcj'Zp)~X6maDwfW@localhost:27017/"
+export MONGODB_URI="mongodb://admin:bgKdk,Qcj'Zp)~X6maDwfW@localhost:9631/"
 python3 main.py &
 wait
